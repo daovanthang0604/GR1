4e02166c98a973c416ae322fcc73ca52df201778
Thăng Đào <daovanthang0604@gmail.com>
2022-07-10 16:39:27 +0700

    update calendar and task

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,20 +1,33 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import UserImg from "./../assets/user.jpg";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
-import { users,tasks } from "./../data";
+import { users, tasks } from "./../data";
 import { format, getDaysInMonth } from "date-fns";
 const Calendar = () => {
   // get today date
   const dt = new Date();
-  const [day,setDay] = useState(dt.getDate());
-  const [month,setMonth] = useState(dt.getMonth());
-  const [year,setYear] = useState(dt.getFullYear());
-  console.log(day, month, year);
+  const [day, setDay] = useState(dt.getDate());
+  const [month, setMonth] = useState(dt.getMonth());
+  const [year, setYear] = useState(dt.getFullYear());
+  // get number of days in a month
   const daysInMonth = getDaysInMonth(new Date(year, month));
-  console.log(format(new Date(year, month), "LLLL"));
   const days = [];
   for (let i = 1; i <= daysInMonth; i++) days.push(i);
-  const colors = ['sun', 'accent', 'pool', 'tertiary']
+  // when user want to see previous month in calendar
+  const handlePrevMonth = (curMonth) => {
+    let updateMonth = curMonth - 1;
+    if (updateMonth == -1) {
+      setMonth(11);
+      setYear(year - 1);
+    } else setMonth(updateMonth);
+  };
+  const handleNextMonth = (curMonth) => {
+    let updateMonth = curMonth + 1;
+    if (updateMonth == 12) {
+      setMonth(0);
+      setYear(year + 1);
+    } else setMonth(updateMonth);
+  };
   return (
     <section className="pt-8 overflow-hidden flex flex-col">
       {/* current month and year */}
@@ -24,67 +37,117 @@ const Calendar = () => {
           "MMMM"
         )} ${year}`}</span>
         <div className="flex justify-around items-center space-x-2">
-          <ChevronLeftIcon className="w-4 h-4 text-gray-400 cursor-pointer" onClick={()=>setMonth(month-1)}/>
-          <ChevronRightIcon className="w-4 h-4 text-gray-400 cursor-pointer" onClick={()=>setMonth(month+1)}/>
+          <ChevronLeftIcon
+            className="w-4 h-4 text-gray-400 cursor-pointer"
+            onClick={() => handlePrevMonth(month)}
+          />
+          <ChevronRightIcon
+            className="w-4 h-4 text-gray-400 cursor-pointer"
+            onClick={() => handleNextMonth(month)}
+          />
         </div>
       </div>
       {/* calendar */}
       <div className="pt-8 flex flex-col space-x-4 overflow-x-scroll overflow-y-scroll snap-x scrollbar">
         <div className="pt-8 space-y-4">
-        <div className="grid grid-flow-col auto-cols-auto h-12 ml-64">
-          {days.map((day) => (
-            <div className="w-24 px-8  border-x border-solid border-transparent whitespace-nowrap">
-              <span>{`${format(
-              new Date(year, month, day),
-              "EEEEE d"
-            )}`}</span>  
-            </div>
-          ))}
-        </div>
-          {users.map((user,index) => {
-            let randColor = colors[Math.floor(Math.random()*colors.length)];
+          <div className="grid grid-flow-col auto-cols-auto h-12 ml-64">
+            {days.map((day) => (
+              <div className="w-24 px-8  border-x border-solid border-transparent whitespace-nowrap">
+                <span
+                  className={`${
+                    day == dt.getDate() &&
+                    month == dt.getMonth() &&
+                    year == dt.getFullYear() &&
+                    "text-ocean"
+                  }`}
+                >{`${format(new Date(year, month, day), "EEEEE d")}`}</span>
+              </div>
+            ))}
+          </div>
+          {/* get each user calendar */}
+          {users.map((user, index) => {
+            // set colors for each user
+            let randColor = "";
+            if (index % 4 == 1) randColor = "bg-pool";
+            else if (index % 4 == 2) randColor = "bg-sun";
+            else if (index % 4 == 3) randColor = "bg-tertiary";
+            else if (index % 4 == 0) randColor = "bg-accent";
             let misson = [];
-            let startDate, endDate = null;
+            let startDate,
+              endDate = null;
             const { id, name, image, job } = user;
-            const curTask = tasks.find(task=>task.id == id);
-            console.log(curTask);
-            if(curTask) 
-            {misson = curTask.misson
-             startDate = misson[0].startDate
-             endDate = misson[0].endDate
+            const curTask = tasks.find((task) => task.id == id);
+            if (curTask) {
+              misson = curTask.misson;
             }
-            const result1 = new Date(startDate).getDate();
-            const result2 = new Date(endDate).getDate();
-            console.log(result1);
-            
+
             return (
               <div className="grid grid-flow-col auto-rows-auto">
-
-              <div
-                className="flex space-x-4 items-center border border-solid border-slate-200 py-2 px-8 rounded-xl w-64"
-                key={id}
-              >
-                <img
-                  src={image}
-                  alt="user profile"
-                  className="w-10 h-10 rounded-full"
-                />
-                <div>
-                  <h5 className="font-semibold">{name}</h5>
-                  <span className="font-normal text-gray-400">{job}</span>
+                <div
+                  className="flex space-x-4 items-center border border-solid border-slate-200 py-2 px-8 rounded-xl w-64"
+                  key={id}
+                >
+                  <img
+                    src={image}
+                    alt="user profile"
+                    className="w-10 h-10 rounded-full"
+                  />
+                  <div>
+                    <h5 className="font-semibold">{name}</h5>
+                    <span className="font-normal text-gray-400">{job}</span>
+                  </div>
                 </div>
-              </div>
-              {days.map((day)=><div key={day} className={`w-24 bg-navi flex justify-center items-center relative after:h-[125%] ${index == users.length-1 && ("after:h-[100%]")} after:absolute after:w-full after:border-x after:border-solid after:border-slate-200 tester`}>
-                {misson.map(mis=>{
-                  let numberDate = new Date(mis.endDate).getDate() - new Date(mis.startDate).getDate() + 1;
-                  console.log( new Date(mis.endDate).getDate() - new Date(mis.startDate).getDate() + 1)
-                  // if(new Date(mis.startDate).getDate() == day && new Date(mis.endDate).getDate() == day) return <div className="w-full h-3/5 bg-accent z-10 rounded-l-2xl rounded-r-2xl" ></div>
-                  // if(new Date(mis.startDate).getDate() == day) return <div className="w-full h-3/5 bg-accent z-10 rounded-l-2xl" ></div>
-                  // if(new Date(mis.endDate).getDate() == day) return <div className="w-full h-3/5 bg-accent z-10 rounded-r-2xl" ></div>
-                  console.log(month)
-                  if(new Date(mis.startDate).getDate() <= day && day <= new Date(mis.endDate).getDate() && month == new Date(mis.startDate).getMonth()) return <div className={`w-full h-3/5 bg-${randColor} z-10 flex items-center justify-center whitespace-nowrap overflow-x-visible ${new Date(mis.startDate).getDate() == day && ("rounded-l-2xl z-20")} ${new Date(mis.endDate).getDate() == day && ("rounded-r-2xl")} text-white relative`}> <span className={`absolute top-2/4 translate-y-[-50%] left-5 w-[calc(96px*0.8*${numberDate})] overflow-hidden text-ellipsis`}>{(new Date(mis.startDate).getDate() == day) && mis.name}</span></div>
-                })}
-              </div>)}
+                {days.map((day) => (
+                  <div
+                    key={day}
+                    className={`w-24 bg-navi flex justify-center items-center relative after:h-[135%] ${
+                      index == users.length - 1 && "after:h-[100%]"
+                    } after:absolute after:w-full after:border-x after:border-solid after:border-slate-200 tester`}
+                  >
+                    {misson.map((mis) => {
+                      let numberDate =
+                        new Date(mis.endDate).getDate() -
+                        new Date(mis.startDate).getDate() +
+                        1;
+                      console.log(
+                        new Date(mis.endDate).getDate() -
+                          new Date(mis.startDate).getDate() +
+                          1
+                      );
+                      // if(new Date(mis.startDate).getDate() == day && new Date(mis.endDate).getDate() == day) return <div className="w-full h-3/5 bg-accent z-10 rounded-l-2xl rounded-r-2xl" ></div>
+                      // if(new Date(mis.startDate).getDate() == day) return <div className="w-full h-3/5 bg-accent z-10 rounded-l-2xl" ></div>
+                      // if(new Date(mis.endDate).getDate() == day) return <div className="w-full h-3/5 bg-accent z-10 rounded-r-2xl" ></div>
+                      console.log(month);
+                      if (
+                        new Date(mis.startDate).getDate() <= day &&
+                        day <= new Date(mis.endDate).getDate() &&
+                        month == new Date(mis.startDate).getMonth() &&
+                        year == new Date(mis.startDate).getFullYear()
+                      )
+                        return (
+                          <div
+                            className={`w-full h-4/6 ${randColor} z-10 flex items-center justify-center whitespace-nowrap overflow-x-visible ${
+                              new Date(mis.startDate).getDate() == day &&
+                              "rounded-l-2xl z-20"
+                            } ${
+                              new Date(mis.endDate).getDate() == day &&
+                              "rounded-r-2xl"
+                            } text-white relative`}
+                          >
+                            {new Date(mis.startDate).getDate() == day && (
+                              <span className="w-2 h-2 bg-white cursor-pointer rounded-full absolute left-2 after:w-4 after:h-4 after:rounded-full after:border after:border-slate-200 after:absolute after:top-[-0.25rem] after:left-[-0.25rem]"></span>
+                            )}
+                            <span
+                              className={`absolute top-2/4 translate-y-[-50%] left-6 w-[calc(96px*0.8*${numberDate})] overflow-hidden text-ellipsis`}
+                            >
+                              {new Date(mis.startDate).getDate() == day &&
+                                mis.name}
+                            </span>
+                          </div>
+                        );
+                    })}
+                  </div>
+                ))}
               </div>
             );
           })}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import {
   DatabaseIcon,
   ViewGridIcon,
@@ -12,67 +12,110 @@ import {
   PlusIcon,
   ChevronUpIcon,
   ChevronDoubleRightIcon,
+  LogoutIcon,
 } from "@heroicons/react/outline";
 import UserImg from "./../assets/user.jpg";
 const NavBar = () => {
-  const [toggle,setToggle] = useState(false);
+  const [toggle, setToggle] = useState(false);
   return (
-    <nav className={`p-8 w-[300px] bg-navi h-screen border-r-2 border-slate-100 relative transition-all ${toggle && ('w-[100px]')}`}>
+    <nav
+      className={`p-8 w-[300px] bg-navi h-screen border-r-2 border-slate-100 relative transition-all ease-out duration-200 ${
+        toggle && "w-[100px]"
+      }`}
+    >
       <div className={`flex items-center align-middle justify-between`}>
         <div className="flex items-center align-middle space-x-4">
           <DatabaseIcon className="w-12 h-12 text-white rounded-full bg-ocean p-2" />
           {!toggle && <h3 className="text-4xl font-semibold">PJ</h3>}
         </div>
-       {!toggle ? <ArrowLeftIcon className="w-6 h-6 font-light text-slate-700 cursor-pointer" onClick={()=>setToggle(!toggle)}/> : <ChevronDoubleRightIcon className="absolute w-6 h-6 text-gray-400 right-[-1rem] cursor-pointer" onClick={()=>setToggle(!toggle)}/> }
+        {!toggle ? (
+          <ArrowLeftIcon
+            className="w-6 h-6 font-light text-slate-700 cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
+        ) : (
+          <ChevronDoubleRightIcon
+            className="absolute w-6 h-6 text-gray-400 right-[-1rem] cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
+        )}
       </div>
       {/* Nav Content */}
       <div className="pt-16 flex flex-col space-y-8 leading-6">
         <div className="flex items-center align-middle space-x-4">
           <ViewGridIcon className="w-8 h-8 text-slate-400" />
-         {!toggle && <span className="text-xl font-medium tracking-tight cursor-pointer">
-            Dashboard
-          </span> }
+          {!toggle && (
+            <span className="text-xl font-medium tracking-tight cursor-pointer">
+              Dashboard
+            </span>
+          )}
         </div>
         <div className="flex items-center align-middle space-x-4">
           <ChartBarIcon className="w-8 h-8 text-slate-400" />
-         {!toggle && <span className="text-xl font-medium tracking-tight cursor-pointer">
-            Analytics
-          </span>}
+          {!toggle && (
+            <span className="text-xl font-medium tracking-tight cursor-pointer">
+              Analytics
+            </span>
+          )}
         </div>
         <div className="flex items-center align-middle space-x-4">
           <FolderIcon className="w-8 h-8 text-slate-400" />
-          {!toggle && <span className="text-xl font-medium tracking-tight cursor-pointer">
-            Projects
-          </span>}
+          {!toggle && (
+            <span className="text-xl font-medium tracking-tight cursor-pointer">
+              Projects
+            </span>
+          )}
         </div>
         <div className="flex items-center align-middle space-x-4">
           <CogIcon className="w-8 h-8 text-slate-400" />
-          {!toggle && <span className="text-xl font-medium tracking-tight cursor-pointer">
-            Settings
-          </span>}
+          {!toggle && (
+            <span className="text-xl font-medium tracking-tight cursor-pointer">
+              Settings
+            </span>
+          )}
         </div>
       </div>
       {/* Create new task */}
-      <div className={`flex justify-center items-center space-x-4 mt-16 bg-faze rounded-xl p-4 ${toggle && ('p-0')}`}>
-       {!toggle && <span className="text-xl w-[120px] block text-left">
-          Create <br /> new task
-        </span> }
-        <PlusIcon className={`w-10 h-10 text-white rounded-full bg-ocean p-1.5 cursor-pointer ${toggle && ('shrink-0')}`} />
+      <div
+        className={`flex justify-center items-center space-x-4 mt-16 bg-faze rounded-xl p-4 ${
+          toggle && "p-0"
+        }`}
+      >
+        {!toggle && (
+          <span className="text-xl w-[120px] block text-left">
+            Create <br /> new task
+          </span>
+        )}
+        <PlusIcon
+          className={`w-10 h-10 text-white rounded-full bg-ocean p-1.5 cursor-pointer ${
+            toggle && "shrink-0"
+          }`}
+        />
       </div>
       {/* User profile */}
       <div className="absolute bottom-8">
-        <div className={`flex justify-around space-x-4 items-center ${toggle && ('space-x-1')}`}>
-          <img
-            src={UserImg}
-            alt="user profile"
-            className="w-10 h-10 rounded-full"
-          />
-         {!toggle && <div>
-            <h5>Finna</h5>
-            <span>finna@gmail.com</span>
-          </div> }
-          <ChevronUpIcon className="w-4 h-4" />
-        </div>
+        {!toggle ? (
+          <div
+            className={`flex justify-around space-x-4 items-center ${
+              toggle && "space-x-1"
+            }`}
+          >
+            <img
+              src={UserImg}
+              alt="user profile"
+              className="w-10 h-10 rounded-full"
+            />
+            <div>
+              <h5>Finna</h5>
+              <span>finna@gmail.com</span>
+            </div>
+            <ChevronUpIcon className="w-4 h-4" />
+          </div>
+        ) : (
+          <div className="flex justify-center items-center w-8 h-8 text-slate-400 cursor-pointer">
+            <LogoutIcon />
+          </div>
+        )}
       </div>
     </nav>
   );
